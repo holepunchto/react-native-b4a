3af2fecbbddc231737a0169f8a41eefbfe6fc869
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-09-25 11:29:26 +0200

    Optimise base64 and hex routines (#2)

--- specs/NativeBufferForArray.ts
+++ specs/NativeBufferForArray.ts
@@ -12,6 +12,33 @@ export interface Spec extends TurboModule {
     length: number,
     string: string
   ): number
+
+  toStringUTF16(buffer: Object, offset: number, length: number): string
+
+  writeUTF16(
+    buffer: Object,
+    offset: number,
+    length: number,
+    string: string
+  ): number
+
+  toStringBase64(buffer: Object, offset: number, length: number): string
+
+  writeBase64(
+    buffer: Object,
+    offset: number,
+    length: number,
+    string: string
+  ): number
+
+  toStringHex(buffer: Object, offset: number, length: number): string
+
+  writeHex(
+    buffer: Object,
+    offset: number,
+    length: number,
+    string: string
+  ): number
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('BufferForArray')
